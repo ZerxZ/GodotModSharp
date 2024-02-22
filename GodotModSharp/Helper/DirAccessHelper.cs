3a233446a0a3672882b1e23c078c069ad3a38e72
深渊の鸽子 <ghzero97@gmail.com>
2024-02-22 17:48:28 +0100

    Refactor DirAccessFilesEnumerator in DirAccessHelper.cs

--- GodotModSharp/Helper/DirAccessHelper.cs
+++ GodotModSharp/Helper/DirAccessHelper.cs
@@ -9,25 +9,16 @@ public static class DirAccessHelper
 
     public static DirAccessFilesEnumerator GetFiles(this DirAccess dirAccess, [RegexPattern] string expression) => new DirAccessFilesEnumerator(dirAccess, expression);
 
-    public ref struct DirAccessFilesEnumerator
+    public ref struct DirAccessFilesEnumerator(DirAccess dirAccess, [RegexPattern] string expression)
     {
-        private          DirAccess    _dirAccess;
-        private          string       _expression;
-        public           List<string> Current          { get; private set; }
-        private          List<string> CurrentDirectory { get; set; } = new List<string>(128);
-        private          bool         _initialized = false;
-        private          List<string> newDirectory;
-        private readonly List<string> fileList;
-        public DirAccessFilesEnumerator(DirAccess dirAccess, [RegexPattern] string expression)
-        {
-            _dirAccess = dirAccess;
-            _expression = expression;
-            Current = new List<string>(128);
-            CurrentDirectory = new List<string>(128);
-            newDirectory = new List<string>(128);
-            fileList = new List<string>(128);
-        }
-        public DirAccessFilesEnumerator GetEnumerator() => this;
+        private          string                   _expression = expression;
+        private          Regex                    regex       = new Regex(expression, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        public           List<string>             Current          { get; private set; } = new List<string>();
+        private          List<string>             CurrentDirectory { get; set; }         = new List<string>();
+        private          bool                     _initialized = false;
+        private          List<string>             newDirectory = new List<string>();
+        private readonly List<string>             fileList     = new List<string>();
+        public           DirAccessFilesEnumerator GetEnumerator() => this;
         public bool TryGetFiles(List<string> dir, DirAccess dirAccess, string currentDirectoryPath, Regex regex)
         {
             var files = dirAccess.GetFiles();
@@ -64,24 +55,22 @@ public static class DirAccessHelper
             {
                 return false;
             }
-            var    regex = new Regex(_expression, RegexOptions.IgnoreCase | RegexOptions.Compiled);
             string currentDirectoryPath;
             if (!_initialized)
             {
                 _initialized = true;
-                currentDirectoryPath = _dirAccess.GetCurrentDir();
-                if (!TryGetFiles(CurrentDirectory,_dirAccess,currentDirectoryPath,regex))
+                currentDirectoryPath = dirAccess.GetCurrentDir();
+                if (!TryGetFiles(CurrentDirectory, dirAccess, currentDirectoryPath, regex))
                 {
                     return false;
                 }
             }
 
-
             foreach (var directory in CurrentDirectory)
             {
-                
+
                 using var currentDirAccess = DirAccess.Open(directory);
-              
+
                 if (currentDirAccess == null)
                 {
                     continue;
